0f8b9b24d80dd290dab30a42df233270c33db36b
ShivamCo <shivam.singh.coo@gmail.com>
2026-05-31 12:00:34 +0530

    Factorial

---
+++ 3_basic_math/2_factorial_of_a_Number.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    # Write your code here
+    # Print the factorial value.
+
+    
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+    
+
+    
+
+print(factorial(11))
+
+
+
