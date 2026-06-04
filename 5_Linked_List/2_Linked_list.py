3d13c666f852a3c876540bd08599303752eaafa5
ShivamCo <shivam.singh.coo@gmail.com>
2026-06-04 18:27:57 +0530

    Singly Linked List

--- 5_Linked_List/2_Linked_list.py
+++ 5_Linked_List/2_Linked_list.py
@@ -10,6 +10,24 @@ class SinglyLinkedList:
     def __init__(self, head=None):
         self.head = head
 
+    def insertAtBegn(self, value):
+        temp = Node(value)
+
+        temp.next = self.head
+        self.head = temp
+
+    def insertAtMid(self, value, loc):
+        temp = Node(value)
+        t1 = self.head
+
+        while(t1.next != None):
+            if( t1.info == loc ):
+                temp.next = t1.next
+                t1.next = temp        
+                
+            t1 = t1.next
+
+
     def insertAtEnd(self, value):
         temp = Node(value)
 
@@ -24,6 +42,31 @@ class SinglyLinkedList:
 
         t1.next = temp
 
+    def deleteElement(self, value):
+        
+        t1 = self.head
+        prev = t1
+
+        if (t1.info == value ):
+            self.head = t1.next
+        
+        
+        
+        while t1.next != None:
+
+            
+            if (t1.info == value):
+                prev.next = t1.next
+                break
+
+            
+            else: 
+                prev = t1
+                t1 = t1.next
+
+        if (t1.next == None):
+            prev.next = None
+
     def printLinkedList(self):
         t1 = self.head
 
@@ -31,11 +74,19 @@ class SinglyLinkedList:
             print(t1.info)
             t1 = t1.next
 
+    
+
 
 obj = SinglyLinkedList()
 
 obj.insertAtEnd(10)
 obj.insertAtEnd(20)
 obj.insertAtEnd(30)
+
+obj.insertAtBegn(21)
+
+obj.insertAtMid(211, 20) # 211 after 20(not index element)
+
+obj.deleteElement(30)
 
 obj.printLinkedList()
